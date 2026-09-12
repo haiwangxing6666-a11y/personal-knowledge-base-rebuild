70105c419405b3f5a6dd63f9ec60b964891408de
haiwangxing6666-a11y <haiwangxing6666@163.com>
2026-09-12 18:52:53 +0800

    feat: 建立资料数据模型与数据库访问

---
+++ src/main/java/com/ithwx/personalknowledgebase/repository/DocumentRepository.java
@@ -0,0 +1,13 @@
+package com.ithwx.personalknowledgebase.repository;
+
+import com.ithwx.personalknowledgebase.entity.DocumentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<DocumentEntity, Long> {
+
+    List<DocumentEntity> findAllByOrderByUploadTimeDesc();
+}
